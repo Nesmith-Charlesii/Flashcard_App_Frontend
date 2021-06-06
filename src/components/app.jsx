f268c21d257e6df8b7a7d9014e97a6531ebf2e19
Nessiah919 <nesmith.charlesii@gmail.com>
2021-06-05 20:33:36 -0400

    created 'App' component. imported App component to index.js

---
+++ src/components/app.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>TEST</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
